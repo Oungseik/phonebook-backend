28974da3a2f8ec332ab52b36bf01ee562469fd15
Oungseik <mhemaungthuwin@gmail.com>
2022-08-22 13:01:50 +0630

    feat: prevent posting with no data

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 // @ts-check
 
 const express = require("express");
-const app = express();
+
 const { data } = require("./data.js");
+let contacts = data;
 
+const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("<h1>Welcome to Phonebook Application");
 });
 
-
-app.get("/api/persons", (req, res) => {
-  res.json(data);
+app.get("/api/persons", (_req, res) => {
+  res.json(contacts);
 });
 
-app.get("/info", (req, res) => {
+app.get("/info", (_req, res) => {
   res.write(`<p>Phonebook has info for ${data.length} people</p>`);
   res.write(`${new Date().toString()}`);
 });
@@ -30,6 +32,41 @@ app.get("/api/persons/:id", (req, res) => {
   res.json(contact);
 })
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  contacts = data.filter(datum => datum.id !== id);
+  res.status(204).end();
+});
+
+const generateID = () => {
+  const id = contacts.length > 0 ? Math.max(...contacts.map(contact => contact.id)) : 0;
+  return id + 1;
+}
+
+app.post("/api/persons", (req, res) => {
+  /** @type {{name: string, number: string}}*/
+  const { name, number } = req.body;
+
+  if (!name) {
+    return res.status(400).json({
+      error: "name is missing"
+    })
+  }
+
+  if (!number) {
+    return res.status(400).json({
+      error: "number is missing"
+    })
+  }
+
+  const person = {
+    name, number, id: generateID(),
+  }
+
+  contacts.push(person);
+  res.json(person);
+
+})
 
 
 const PORT = 3001;
